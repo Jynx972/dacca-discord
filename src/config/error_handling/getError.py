3133617bc4df559ab575283b964ef98fb400af23
Jynx972 <screaminggoats@hotmail.com>
2026-05-30 11:35:20 +0930

    Hard coding dac error file path
    Missed this one when I did Issue #6

--- src/config/error_handling/getError.py
+++ src/config/error_handling/getError.py
@@ -1,20 +1,24 @@
 import random
 import discord
 
+
 def getErrMsg():
-    errorMsgs = []
-    with open('errors.txt') as file:
-        for line in file:
-            errorMsgs.append(line.rstrip())
-    file.close()
-    retIdx = random.randint(0,len(errorMsgs))
-    errEmbed = discord.Embed(title="Error",description=errorMsgs[retIdx],color=0xFF0000)
-    return(errEmbed)
+	errorMsgs = []
+	with open("./src/config/error_handling/errors.txt") as file:
+		for line in file:
+			errorMsgs.append(line.rstrip())
+	file.close()
+	retIdx = random.randint(0, len(errorMsgs))
+	errEmbed = discord.Embed(
+		title="Error", description=errorMsgs[retIdx], color=0xFF0000
+	)
+	return errEmbed
+
 
 def adminError(errIdx):
-    errorMsgs = []
-    with open('errors.txt') as file:
-        for line in file:
-            errorMsgs.append(line.rstrip())
-    file.close()
-    return(errorMsgs[int(errIdx)])
+	errorMsgs = []
+	with open("./src/config/error_handling/errors.txt") as file:
+		for line in file:
+			errorMsgs.append(line.rstrip())
+	file.close()
+	return errorMsgs[int(errIdx)]
